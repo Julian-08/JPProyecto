e3203c133aecdec377e6fa47857c8b83ab423567
Cesarcardozo11 <111097562+Cesarcardozo11@users.noreply.github.com>
2025-05-18 22:35:59 -0500

    se creo la clase view del cliente

--- src/view/ClienteView.java
+++ src/view/ClienteView.java
@@ -11,5 +11,11 @@ public class ClienteView {
         System.out.println("Ingrese el nombre del cliente");
         String nombre = teclado.nextLine();
         System.out.println("Numero de identificacion del cliente");
+        String numeroIdentificacion = teclado.nextLine();
+        System.out.println("Ingrese el tipo de identificacion");
+        String tipoDeIdentificacion =  teclado.nextLine();
+        System.out.println("Ingrese el correo del cliente");
+        String correo = teclado.nextLine();
+        
     }
 }
