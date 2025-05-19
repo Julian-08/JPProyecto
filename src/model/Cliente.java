2ecc86e2bc1141f38d9dc7b33d116f6a373efd4a
Julian Barreto <140745766+Julian-08@users.noreply.github.com>
2025-05-18 23:15:19 -0500

    Actualizacion Cliente model

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -6,15 +6,21 @@ public class Cliente {
     private String tipoDeIdentificacion;
     private String correo;
     private String direccion;
+    private Cliente[] matrizClientes;
+    private int capacidadMaximaClientes;
+    private int cantidadClientes;
 
     
     public Cliente(String nombre, String numeroIdentificacion, String tipoDeIdentificacion, String correo,
-            String direccion) {
+            String direccion, int capacidadMaximaClientes, int i, int j) {
         this.nombre = nombre;
         this.numeroIdentificacion = numeroIdentificacion;
         this.tipoDeIdentificacion = tipoDeIdentificacion;
         this.correo = correo;
         this.direccion = direccion;
+        this.capacidadMaximaClientes = capacidadMaximaClientes;
+        this.matrizClientes = new Cliente[capacidadMaximaClientes];
+        this.cantidadClientes = 0;
     }
 
 
@@ -67,5 +73,40 @@ public class Cliente {
         this.direccion = direccion;
     }
 
-    
+    public boolean crearCliente(Cliente cliente) {
+        if (cantidadClientes < capacidadMaximaClientes) {
+            matrizClientes[cantidadClientes] = cliente;
+            cantidadClientes++;
+            return true;
+        } 
+            return false;
+        }
+        
+    public Cliente obtenerCliente(String idCliente) {
+        for (int i = 0; i < cantidadClientes; i++) {
+            if (matrizClientes[i].getNumeroIdentificacion().equals(idCliente)) {
+                return matrizClientes[i];
+            }
+        }
+        return null;
+    }
+
+    public boolean actualizarCliente(Cliente clienteActualizado) {
+        for (int i = 0; i < matrizClientes.length; i++) {
+            if (matrizClientes[i].getNumeroIdentificacion().equals(clienteActualizado.getNumeroIdentificacion())) {
+                matrizClientes[i].setNombre(clienteActualizado.getNombre());
+                matrizClientes[i].setDireccion(clienteActualizado.getDireccion());
+                matrizClientes[i].setCorreo(clienteActualizado.getCorreo());
+                matrizClientes[i].setTipoDeIdentificacion(clienteActualizado.getTipoDeIdentificacion());
+                return true;
+            }
+        }
+        return false;
+    }
+    public Cliente[] obtenerTodosClientes() {
+        Cliente[] clientesActuales = new Cliente[cantidadClientes];
+        System.arraycopy(matrizClientes, 0, clientesActuales, 0, cantidadClientes);
+        return clientesActuales;
+    }
+    //Metodos similares para Registrador, consumo, etc. (con sus respectivas matrices)
 }
