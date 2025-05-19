2374123c05ffb66370b467711ad23e09d5110b1a
Cesarcardozo11 <111097562+Cesarcardozo11@users.noreply.github.com>
2025-05-19 00:06:46 -0500

    prueba

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,5 @@
 
-import model.Cliente; // Importación de model.Cliente
-import view.ClienteView; // Importación de view.ClienteView
-import controller.FranjasControlador; // Importación de controller.FranjasControlador
+ // Importación de controller.FranjasControlador
 
 
 
@@ -10,7 +8,7 @@ public class Principal {
        
 
     public static void main(String[] args) {
-        ClienteView objClienteView = new ClienteView(); // Instancia de ClienteView
+        
          // Llamada al método mPedirDatos() de ClienteView
     }
 }
